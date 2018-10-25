e6d859f14afb660ec29b8703da1e0f93cc73e167
andole87 <andole87@gmail.com>
2018-10-25 14:53:43 +0900

    audio_detail 모델 추가, play pause 버튼 추가, currentTime 표시

--- audio/models.py
+++ audio/models.py
@@ -18,3 +18,25 @@ class Audio(models.Model):
     def __str__(self):
         return self.title
 
+class Audio_Detail(models.Model):
+    detail_parent = models.ForeignKey(Audio, on_delete = models.CASCADE)
+    detail_title = models.CharField(max_length=20)
+    detail_author = models.CharField(max_length=20)
+    detail_translator = models.CharField(blank=True, null=True, max_length=20)
+    detail_speaker = models.CharField(max_length=20)
+    detail_pubdate = models.DateTimeField(blank=True, null=True)
+    detail_text = models.TextField()
+    detail_imgsrc = models.CharField(max_length=255)
+    detail_playtime = models.TimeField(blank=True)
+    detail_filesize = models.CharField(null=True, max_length=20)
+    detail_price = models.DecimalField(max_digits=5, decimal_places=1,blank=True,null=True)
+    detail_outline = models.TextField()
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.detail_title
+
+    
+
