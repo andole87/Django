02fd23ad4787c18225c0664cd36f7e4539823d02
Ubuntu <ubuntu@ip-172-31-25-4.ap-northeast-2.compute.internal>
2018-10-30 12:37:56 +0000

    EC2 settings.py 조정

--- mysite/settings.py
+++ mysite/settings.py
@@ -150,6 +150,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+        '/srv/Django/audio/static',
+        '/srv/Django/admin/static',
+        )
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
